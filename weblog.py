a3a9afa0e95f1b121cb44d04a8803fa92c64d5c1
chenmeng <823473559@qq.com>
2022-04-10 15:15:47 +0800

    Update README.md

--- weblog.py
+++ weblog.py
@@ -1,14 +1,20 @@
-import socket
-import time
+# import socket
+
 import logging
 
 
+logging.basicConfig(filename=r"./bin/log.txt",format='%(asctime)s %(message)s',filemode='w')
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
 def send_server():
-    socket1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    socket1.connect(('101.34.28.245',8000))
+    # socket1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # socket1.connect(('101.34.28.245',8000))
     while True:
         message = yield
-        socket1.send(b'%s' % message)
+        message = str(message)
+        logger.info(message)
 
 WebSocket = send_server()
 WebSocket.send(None)
